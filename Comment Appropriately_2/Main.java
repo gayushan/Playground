22533c57bc7f938b4a987d168f7f5ea2a15f3d76
Gayathri Shanmuganathan <71088954+gayushan@users.noreply.github.com>
2021-06-04 11:25:18 +0530

    Test code pushed to repository.

---
+++ Comment Appropriately_2/Main.java
@@ -0,0 +1,14 @@
+#This code prints the desired output
+#This line is supposed to be a comment
+'''
+
+Once the comments are redesigned
+
+
+'''
+
+n=int(input()) 							#This line takes the inputs
+prod=1									#This line creates a varibale to store the product
+for i in range(1,n+1):					#Looping element
+  prod=prod*i								
+print(prod)								#Printing elemet	
